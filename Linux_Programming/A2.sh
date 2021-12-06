5e862bd98206037f9b1ba5d03b86f820958775cf
Keagan Rodrigues <keaganrodrigues7@gmail.com>
2021-12-06 18:06:01 -0500

    A2.sh Created

--- Linux_Programming/A2.sh
+++ Linux_Programming/A2.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 echo "The script PID is $$" #$$ is the Process ID of the current running process
 sleep 30 & #30 second pause to not overwhelm the execution
-child_pid = "$!" #Defining the Process ID of the most recent background process
-echo "Child PID is $child_pid" #Displays the PID of the current child process
+child_PID = "$!" #Defining the Process ID of the most recent background process
+echo "Child PID is $child_PID" #Displays the PID of the current child process
+echo "Child process with PID $child_PID is now in the OS waiting queue" #Prints the message
+echo "The child process if waiting for a software interrutp form the user."
+echo "Enger the SIGNALINTeGER inerterupt to habe the child process exdecute on the CPU."
+kill $child_PID
