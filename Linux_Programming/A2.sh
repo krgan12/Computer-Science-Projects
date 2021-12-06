6d13c3d0656422740afadb12c2eccafd6e1920e1
Keagan Rodrigues <keaganrodrigues7@gmail.com>
2021-12-06 15:05:30 -0500

    stage2test

--- Linux_Programming/A2.sh
+++ Linux_Programming/A2.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 echo "The script PID is $$" #$$ is the Process ID of the current running process
-sleep 30 &
-child_pid = "$!"
+sleep 30 & #30 second pause to not overwhelm the execution
+child_pid = "$!" #Defining the Process ID of the most recent background process
+echo "Child PID is $child_pid" #Displays the PID of the current child process
