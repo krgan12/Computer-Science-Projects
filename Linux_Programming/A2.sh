81744f7ee80b39a713bf411c40a72e25543d5138
Keagan Rodrigues <keaganrodrigues7@gmail.com>
2021-12-07 10:10:22 -0500

    A2.sh Modified

--- Linux_Programming/A2.sh
+++ Linux_Programming/A2.sh
@@ -6,9 +6,9 @@ echo "Child PID is $Child_PID" #Displays the PID of the current child process
 echo "Child process with PID $Child_PID is now in the OS waiting queue" #Prints the message
 echo "The child process if waiting for a software interrutp form the user." #Echos the message to the output on terminal
 echo "Enger the SIGNALINTeGER inerterupt to habe the child process exdecute on the CPU."
-kill -2 $Child_PID
-trap kill -9
-echo "Enter SIGQUIT internrtupt."
-kill $Child_PID
-trap kill -9
-trap echo "SIGQUIT Siggal recviered!" SIGquTI
+kill -2 $Child_PID #Moves child process from running state to the waiting state
+trap kill -9 #traps the process of the move
+echo "Enter SIGQUIT internrtupt." #Prints to the stdout
+kill $Child_PID #Moves the child process again
+trap kill -9 #traps the process of the move
+
