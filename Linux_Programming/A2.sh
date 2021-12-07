96364e8516f2907b795d66cfcd2164b99874eaab
Keagan Rodrigues <keaganrodrigues7@gmail.com>
2021-12-07 14:42:01 -0500

    A2.sh Modified

--- Linux_Programming/A2.sh
+++ Linux_Programming/A2.sh
@@ -2,13 +2,16 @@
 echo "The script PID is $$" #$$ is the Process ID of the current running process
 sleep 30 & #30 second pause to not overwhelm the execution
 Child_PID = "$!" #Defining the Process ID of the most recent background process
-echo "Child PID is $Child_PID" #Displays the PID of the current child process
-echo "Child process with PID $Child_PID is now in the OS waiting queue" #Prints the message
+#Displays the PID of the current child process
+echo "Child PID is $!Child_PID" 
+echo "Child process with PID $!Child_PID is now in the OS waiting queue" #Prints the message
 echo "The child process if waiting for a software interrutp form the user." #Echos the message to the output on terminal
 echo "Enger the SIGNALINTeGER inerterupt to habe the child process exdecute on the CPU."
-kill -2 $Child_PID #Moves child process from running state to the waiting state
-trap kill -9 #traps the process of the move
+kill -2 $!Child_PID #Moves child process from running state to the waiting state
+trap kill -1 #traps the process of the move
 echo "Enter SIGQUIT internrtupt." #Prints to the stdout
-kill $Child_PID #Moves the child process again
-trap kill -9 #traps the process of the move
-
+kill $!Child_PID #Moves the child process again
+trap kill -1 #traps the process of the move
+echo "Completed executing."
+echo "Terminating all processes."
+exit 
