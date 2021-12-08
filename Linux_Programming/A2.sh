3efbfa7c99f0aaa671337af63b9f1213a34c6a5f
Keagan Rodrigues <keaganrodrigues7@gmail.com>
2021-12-07 21:43:33 -0500

    A2.sh Modified

--- Linux_Programming/A2.sh
+++ Linux_Programming/A2.sh
@@ -7,11 +7,11 @@ echo "Child PID is $Child_PID"
 echo "Child process with PID $Child_PID is now in the OS waiting queue." #Prints the message
 echo "The child process is waiting for a software interrupt from the user." #Echos the message to the output on terminal
 echo "Enger the SIGINT interrupt to have the child process execute on the CPU."
-kill -2 $Child_PID #Moves child process from running state to the waiting state
-trap kill -1 #traps the process of the move
+kill $Child_PID #Moves child process from running state to the waiting state
+trap kill #traps the process of the move
 echo "Enter SIGQUIT interrupt." #Prints to the stdout
 kill $Child_PID #Moves the child process again
-trap kill -1 #traps the process of the move
+trap kill #traps the process of the move
 echo "Completed executing." #Prints 'Completed executing.' to the stdout
 echo "Terminating all processes." #Prints 'Terminating all processes.' to the stdout
-exit 1 #Exits out of the program with no errors
+exit 2 #Exits out of the program with no errors
