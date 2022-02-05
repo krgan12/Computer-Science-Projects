99b7316c9b4ec5e679857890798209c72feddbe5
Keagan <52428112+krgan12@users.noreply.github.com>
2022-02-05 11:44:56 -0500

    Add files via upload

---
+++ Python/Side Projects/Sending Fake Data to Scam Sites/main.py
@@ -0,0 +1,32 @@
+import requests, random, time
+
+payload = {
+	"login": "test@email.com",
+	"password": "testpass",
+}
+#Discord Scam Site 1: https://disocrds.gift/NverABbCacD
+#Discord Scam Site 2: https://discosb.gift/X0kD211HPMjf
+#
+def post_request(payload):
+    response = requests.post('https://disocrds.gift/NverABbCacD',data=payload) #This discord scam site is going to be used to save people's data, you can use any other site here.
+    print(payload)
+    print(response)
+    return 
+
+def main():
+    new_payload = payload
+    with open('usernames.txt','r') as username_file:
+        usernames = username_file.readlines()
+    with open('passwords.txt' ,'r') as password_file:
+        passwords = password_file.readlines()
+    for i in range(0, len(usernames)):
+        new_payload['login'] = (usernames[i].rstrip() + str(random.randint(1960, 2021)) + '@gmail.com') #[i] refers to the iteration position, for example, starting at 0, it will be [0] of usernames which will be the first element on the list, second element, and so forth.
+        new_payload['password'] = passwords[i].rstrip()
+        print(f'Sending fake data... Iteration number: {(i+1)}')
+        post_request(new_payload)
+        sleep_time = random.randint(0, 30)
+        print(f'Sleeping for {sleep_time} seconds!')
+        time.sleep(sleep_time)
+        
+while True:
+    main()
