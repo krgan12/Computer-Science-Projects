da7a06a50a920a8e657ea448be4e9dc65ddf1534
Keagan <52428112+krgan12@users.noreply.github.com>
2022-02-13 09:37:29 -0500

    Create BinaryNumbersDisplay.java

---
+++ Java/Side Projects/BinaryNumbersDisplay.java
@@ -0,0 +1,14 @@
+public class my_fun{
+    public static void main (String[] args){
+        int counter;
+        counter = 0;
+        for (counter = 0; counter <= 50; counter++){
+            System.out.println ("00000001111111010101010101000000000");    
+            counter++;        
+            if (counter == 10){
+            System.out.println();
+            counter = 0;
+            }
+        }
+    }
+}
