31d9058cbba26811a27bc3eb301cdeb77a2cfa2a
Keagan <52428112+krgan12@users.noreply.github.com>
2022-02-13 09:38:00 -0500

    Update BinaryNumbersDisplay.java

--- Java/Side Projects/BinaryNumbersDisplay.java
+++ Java/Side Projects/BinaryNumbersDisplay.java
@@ -1,14 +1,21 @@
-public class my_fun{
+public class binary_numbers {
+    
     public static void main (String[] args){
+        
         int counter;
         counter = 0;
         for (counter = 0; counter <= 50; counter++){
+            
             System.out.println ("00000001111111010101010101000000000");    
             counter++;        
             if (counter == 10){
             System.out.println();
             counter = 0;
+                
             }
+            
         }
+        
     }
+    
 }
